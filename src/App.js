d3c0da903582820dc068aa341be468c1fffe5b13
tjs6595 <t.schultejans@schenckprocess.com>
2022-07-16 11:13:53 -0500

    RR-6-Travel Agent Activity Submittal with working BONUS.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import {TailSpin} from 'react-loader-spinner';
+// import AboutUs from './components/AboutUs'
+// import Packages from './components/Packages'
+
 
 function App() {
+  const AboutUs = React.lazy(() => import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<TailSpin type="Bars" color="#FF00FF" height={200} width={200} style={{margin: "0 auto"}}/>}>
+            {/* <h1>Loading.S..</h1> */}
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
